374945c73054b9a0e39f9d6cd649f741a8482140
EKsora <66258685+EKsora@users.noreply.github.com>
2022-07-07 19:06:58 +0800

    light ray pure give ball hit

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -44,10 +44,24 @@ impl Ray {
 }
 
 pub fn ray_color(r:&Ray)->Vec3{
-    let unit_direction=Vec3::new(r.direction().unit().x,r.direction().unit().y,r.direction().unit().z);
+    let unit_direction=Vec3{
+        x:r.direction().unit().x,
+        y:r.direction().unit().y,
+        z:r.direction().unit().z,
+    };
     let t = 0.5*(unit_direction.y + 1.0);
     Vec3::new(1.0, 1.0, 1.0)*(1.0-t) + Vec3::new(0.5, 0.7, 1.0)*t
 }
+
+pub fn hit_sphere(center:Vec3,radius:f64,r:&Ray) -> bool {
+    let oc:Vec3 = r.origin() - center.clone();
+    let a = r.direction()*r.direction();
+    let b =  oc.clone()*r.direction()*2.0;
+    let c = oc.clone()*oc.clone() - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 /*
 pub struct World {
     pub height: u32,
@@ -172,25 +186,23 @@ fn main() {
     render_text(&mut result, msg.as_str());
     result.save("output/test.png").unwrap();
 */
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400.0;
-    let image_height = image_width / aspect_ratio;
+    const aspect_ratio:f64 = 16.0 / 9.0;
+    const image_width:u32 = 400;
+    const image_height:u32 =(image_width as f64/ aspect_ratio)as u32;
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
+    let viewport_height:f64 = 2.0;
+    let viewport_width:f64 = aspect_ratio * viewport_height;
+    let focal_length:f64 = 1.0;
 
-    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let origin = Vec3::zero();
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
     let lower_left_corner = origin.clone() - horizontal.clone()/2.0 - vertical.clone()/2.0 - Vec3::new(0.0, 0.0, focal_length);
     print!("P3\n{} {}\n255\n", image_width, image_height);
-    let mut j=image_height-1.0;
-    let mut i=0.0;
-    while j>=0.0{
-        while i<image_width{
-            let u=i / (image_width-1.0);
-            let v=j / (image_height-1.0);
+    for j in (0..image_height).rev(){
+        for i in (0..image_width){
+            let u:f64=i as f64 / (image_width-1)as f64;
+            let v:f64=j as f64 / (image_height-1)as f64;
             let r=Ray{
                 orig:origin.clone(), 
                 dir:lower_left_corner.clone() + horizontal.clone()*u + vertical.clone()*v - origin.clone(),
@@ -198,7 +210,5 @@ fn main() {
             let pixel_color=ray_color(&r);
             write_color(pixel_color);
         }
-        j-=1.0;i=0.0;
     }
-
 }
--- raytracer/src/ray.rs
+++
@@ -1,2 +0,0 @@
-mod vec3;
-pub use vec3::Vec3;
